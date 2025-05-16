cd07806fd27463610ab9645f1ab5dd4453c4a566
Le Ngoc <nguyenngocle2408@gmail.com>
2025-05-16 23:05:57 +0700

    Initial Commit

---
+++ TakeHome/TakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeHome
+//
+//  Created by Le on 16/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TakeHome/TakeHomeTests/TakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTests.swift
+//  TakeHomeTests
+//
+//  Created by Le on 16/5/25.
+//
+
+import Testing
+@testable import TakeHome
+
+struct TakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
